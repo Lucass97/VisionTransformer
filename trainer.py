4d03ab8cffd06a7c3ce065051ba11fe3a7c6853e
Lucass97 <luca.gregori97@gmail.com>
2025-03-17 20:21:40 +0100

    feat: add save weights capabilities to the trainer

--- trainer.py
+++ trainer.py
@@ -1,3 +1,4 @@
+import os
 import torch
 import torch.nn.functional as F
 from torch.utils.data import DataLoader
@@ -19,16 +20,16 @@ from models.models import ViT
 
 class Trainer:
 
-    def __init__(self, cfg: Config, experiment_name: str) -> None:
+    def __init__(self, cfg: Config, weights_path: str) -> None:
         """
         Initializes the Trainer class with model configuration and experiment settings.
 
         Args:
             cfg (Config): A configuration object containing hyperparameters and settings.
-            experiment_name (str): The name of the current experiment for logging purposes.
         """
-        self.device = cfg.device
-        self.num_epochs = cfg.training.epochs
+
+        self.cfg = cfg
+        self.weights_path = weights_path
 
         # Dataset creation
         self.train_set = create_dataset_instance(cfg.data.class_path, cfg.data.base_path, train=True)
@@ -37,8 +38,6 @@ class Trainer:
         # DataLoader creation
         self.train_loader = DataLoader(self.train_set, cfg.training.batch_size, shuffle=True, num_workers=4)
         self.test_loader = DataLoader(self.test_set, cfg.training.batch_size, shuffle=True, num_workers=4)
-
-        self.patch_size = cfg.model.patch_size
 
         # Model components initialization
         self.cnn_backbone = CNNBackbone(n_channels=cfg.data.n_channels, out_channels=cfg.model.cnn_out_channels).to(cfg.device) if cfg.model.cnn_backbone else None
@@ -55,7 +54,7 @@ class Trainer:
         self.criterion = torch.nn.CrossEntropyLoss()
 
         # TensorBoard logging setup
-        self.tensorboard_writer = TensorboardLogger(log_dir=cfg.logger.base_path, experiment_name=experiment_name, step=cfg.logger.step, max_grid_dim=cfg.logger.max_grid_dim)
+        self.tensorboard_writer = TensorboardLogger(log_dir=cfg.logger.base_path, step=cfg.logger.step, max_grid_dim=cfg.logger.max_grid_dim)
 
         # Custom logger
         self.LOGGER = CustomLogger()
@@ -70,10 +69,10 @@ class Trainer:
         self.model.train()
 
         # Progress bar for training
-        progress_bar = tqdm(total=len(self.train_loader), desc=f"Epoch {epoch}/{self.num_epochs}", position=0, leave=True)
+        progress_bar = tqdm(total=len(self.train_loader), desc=f"Epoch {epoch}/{self.cfg.training.epochs}", position=0, leave=True)
 
         for batch_idx, (inputs, labels) in enumerate(self.train_loader):
-            inputs, labels = inputs.to(self.device), labels.to(self.device)
+            inputs, labels = inputs.to(self.cfg.device), labels.to(self.cfg.device)
 
             n_channels, img_height, img_width = inputs.shape[1], inputs.shape[2], inputs.shape[3]
 
@@ -88,7 +87,7 @@ class Trainer:
 
             # Attention maps processing
             attn_maps = self.model.get_attention_weights()
-            attn_maps = processing(reconstruct_attn_from_patches, batch_idx, self.tensorboard_writer.step, attn_maps[1], (img_height, img_width), n_channels, self.patch_size)
+            attn_maps = processing(reconstruct_attn_from_patches, batch_idx, self.tensorboard_writer.step, attn_maps[1], (img_height, img_width), n_channels, self.cfg.model.patch_size)
 
             # Compute loss and backpropagation
             loss = self.criterion(outputs, labels)
@@ -113,11 +112,11 @@ class Trainer:
         correct = 0
         total = 0
 
-        progress_bar = tqdm(total=len(self.test_loader), desc=f"Epoch {epoch}/{self.num_epochs}", position=0, leave=True)
+        progress_bar = tqdm(total=len(self.test_loader), desc=f"Epoch {epoch}/{self.cfg.training.epochs}", position=0, leave=True)
 
         with torch.no_grad():
             for batch_idx, (inputs, labels) in enumerate(self.test_loader):
-                inputs, labels = inputs.to(self.device), labels.to(self.device)
+                inputs, labels = inputs.to(self.cfg.device), labels.to(self.cfg.device)
 
                 if self.cnn_backbone:
                     inputs = self.cnn_backbone(inputs)
@@ -138,7 +137,7 @@ class Trainer:
 
         avg_loss = running_val_loss / total
         accuracy = correct / total * 100
-        self.LOGGER.info(f"Validation Epoch [{epoch}/{self.num_epochs}]: Loss: {avg_loss:.4f}, Accuracy: {accuracy:.2f}%")
+        self.LOGGER.info(f"Validation Epoch [{epoch}/{self.cfg.training.epochs}]: Loss: {avg_loss:.4f}, Accuracy: {accuracy:.2f}%")
 
         progress_bar.close()
 
@@ -146,9 +145,24 @@ class Trainer:
         """
         Runs the training loop for the specified number of epochs and validates the model 
         at the end of each epoch.
-
         """
-        for epoch in range(1, self.num_epochs + 1):
-            self.LOGGER.info(f"\nStarting epoch {epoch}/{self.num_epochs}")
+
+        if self.weights_path:
+            if os.path.exists(self.weights_path):
+                self.LOGGER.info(f"Loading model weights from {self.weights_path}")
+                self.model.load_state_dict(torch.load(self.weights_path))
+            else:
+                self.LOGGER.warning(f"Model weights file {self.weights_path} not found. Starting from scratch.")
+        
+        for epoch in range(1, self.cfg.training.epochs + 1):
+            self.LOGGER.info(f"Starting epoch {epoch}/{self.cfg.training.epochs}")
+            
             self.train_epoch(epoch)
-            self.validate_epoch(epoch)
+
+            # self.validate_epoch(epoch)
+            
+            # Salvataggio weights
+            if epoch % self.cfg.model_checkpoint.save_freq == 0:
+                model_save_path = os.path.join(self.cfg.model_checkpoint.base_path, f"model_epoch_{epoch}.pt")
+                torch.save(self.model.state_dict(), model_save_path)
+                self.LOGGER.info(f"Model weights saved to {model_save_path}")
